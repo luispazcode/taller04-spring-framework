96dfb630c25a807f220fbb811ee693473ed24735
porta <porta@LUPACO-FX504GD>
2022-03-12 21:16:37 -0500

    Commit initial. Close #1

---
+++ taller4-spring/src/test/java/com/clienteservidor/spingboot/web/app/Taller4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clienteservidor.spingboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller4SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
