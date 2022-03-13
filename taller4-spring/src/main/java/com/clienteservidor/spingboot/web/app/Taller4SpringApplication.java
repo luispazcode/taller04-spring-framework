ed6f3caf04d8112f5258fc055249bd6a291655bc
porta <porta@LUPACO-FX504GD>
2022-03-12 21:16:37 -0500

    Commit initial

---
+++ taller4-spring/src/main/java/com/clienteservidor/spingboot/web/app/Taller4SpringApplication.java
@@ -0,0 +1,13 @@
+package com.clienteservidor.spingboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller4SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller4SpringApplication.class, args);
+	}
+
+}
