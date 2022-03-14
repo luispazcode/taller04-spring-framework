ab4084561866ea9dc2d1178965d24765bd012349
porta <porta@LUPACO-FX504GD>
2022-03-13 20:51:36 -0500

    HTML structure created sucessfully. Close #3

---
+++ taller4-spring/src/main/java/com/clienteservidor/spingboot/web/app/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.clienteservidor.spingboot.web.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/userForm")
+	public String getUserForm() {
+		return "user-form/user-view";
+	}
+}
